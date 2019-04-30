72f22e88b115ea9e01dab206f5c82a1f656d668c
naxus1 <naxus1@gmail.com>
2019-04-30 22:09:22 +0000

    Create point 6

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -5,4 +5,4 @@ for first_digit in range(0, 9):
         if first_digit == 8 and second_digit == 9:
             print("{:d}{:d}".format(first_digit, second_digit))
         else:
-          print("{:d}{:d}, ".format(first_digit, second_digit), end="")
+            print("{:d}{:d}, ".format(first_digit, second_digit), end="")
