234948d67a352ccfc17bc6af30817714f557eb9e
naxus1 <naxus1@gmail.com>
2019-04-30 21:55:45 +0000

    Create point 6

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+for first_digit in range(0, 9):
+    for second_digit in range (first_digit + 1, 10):
+        if first_digit != 8 and second_digit != 9:
+            print("{:d}{:d}, ".format(first_digit, second_digit), end="")
+
+print("{:d}{:d}".format(first_digit, second_digit))
