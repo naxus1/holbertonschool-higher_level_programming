0c5f08c4b1c1f21ee8ab71b6a7f78eb16f65010d
naxus1 <naxus1@gmail.com>
2019-05-01 22:47:51 +0000

    Create point 10

--- 0x01-python-if_else_loops_functions/10-check_cycle.c
+++ 0x01-python-if_else_loops_functions/10-check_cycle.c
@@ -1,20 +1,24 @@
 #include "lists.h"
 #include <stdio.h>
-
+/**
+ * check_cycle - review if linked is a list cycles
+ * @list: list to review
+ * Return: 1 or 0
+ */
 int check_cycle(listint_t *list)
 {
-        listint_t *aux_list1;
-        listint_t *aux_list2;
+	listint_t *list1;
+	listint_t *list2;
 
-        aux_list1 = list;
-        aux_list2 = list;
-        while (aux_list1 != NULL && aux_list2 != NULL && aux_list2->next != NULL)
-        {
-                aux_list1 = aux_list1->next;
-                aux_list2 = aux_list2->next->next;
+	list1 = list;
+	list2 = list;
+	while (list1 != NULL && list2 != NULL && list2->next != NULL)
+	{
+		list1 = list1->next;
+		list2 = list2->next->next;
 
-                if (aux_list1 == aux_list2)
-                        return (1);
-        }
-        return(0);
+		if (list1 == list2)
+			return (1);
+	}
+	return (0);
 }
