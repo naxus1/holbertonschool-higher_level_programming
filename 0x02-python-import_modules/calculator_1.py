7758e4189ce12c7c81e17edfec90551d66e6daa0
naxus1 <naxus1@gmail.com>
2019-05-02 18:57:31 +0000

    Fix

--- 0x02-python-import_modules/calculator_1.py
+++ 0x02-python-import_modules/calculator_1.py
@@ -49,12 +49,3 @@ def div(a, b):
         The return value. a / b
     """
     return int(a / b)
-
-if __name__ == "__main__":
-    from calculator_1 import div, mul, sub, add
-    a = 10
-    b = 5
-    print("{} + {} = {}".format(a, b, add(a, b)))
-    print("{} - {} = {}".format(a, b, sub(a, b)))
-    print("{} * {} = {}".format(a, b, mul(a, b)))
-    print("{} / {} = {}".format(a, b, div(a, b)))
