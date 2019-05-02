ee737588b49ba7992d1dbe7328452598d3a30d56
naxus1 <naxus1@gmail.com>
2019-05-02 19:02:03 +0000

    Add __name__

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -1,14 +1,16 @@
 #!/usr/bin/python3
 from sys import argv
 
-size_argv = len(argv)
+if __name__ == "__main__":
 
-if size_argv > 1:
-    if size_argv - 1 == 1:
-        print("{:d} argument:".format(size_argv - 1))
+    size_argv = len(argv)
+
+    if size_argv > 1:
+        if size_argv - 1 == 1:
+            print("{:d} argument:".format(size_argv - 1))
+        else:
+            print("{:d} arguments:".format(size_argv - 1))
+        for i in range(1, size_argv):
+            print("{}: {}".format(i, argv[i]))
     else:
-        print("{:d} arguments:".format(size_argv - 1))
-    for i in range(1, size_argv):
-        print("{}: {}".format(i, argv[i]))
-else:
-    print("{:d} arguments.".format(size_argv - 1))
+            print("{:d} arguments.".format(size_argv - 1))
