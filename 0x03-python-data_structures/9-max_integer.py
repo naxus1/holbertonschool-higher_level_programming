b65bdcc957e395dd4be397ec7a4a961e5d2d8052
naxus1 <naxus1@gmail.com>
2019-05-07 21:18:23 +0000

    its created if

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def max_integer(my_list=[]):
+    if len(my_list) == 0:
+        return None
     my_list.sort()
     len_my_list = len(my_list) - 1
     return my_list[len_my_list]
