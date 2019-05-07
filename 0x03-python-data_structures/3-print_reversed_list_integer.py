3912eada01b778c1acc64fabe2d05e78d4b3cd71
naxus1 <naxus1@gmail.com>
2019-05-07 18:26:56 +0000

    Modified while

--- 0x03-python-data_structures/3-print_reversed_list_integer.py
+++ 0x03-python-data_structures/3-print_reversed_list_integer.py
@@ -2,6 +2,8 @@
 def print_reversed_list_integer(my_list=[]):
     if my_list is None:
         pass
-    rev_list = my_list[::-1]
-    for i in rev_list:
-        print("{:d}".format(i))
+    else:
+        len_lis = len(my_list) - 1
+        while (len_lis >= 0):
+            print("{:d}".format(my_list[len_lis]))
+            len_lis -= 1
--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     if idx > 0 and idx < len(my_list) - 1:
-        a =  my_list[:]
+        a = my_list[:]
         a[idx] = element
         return (a)
     return (my_list)
