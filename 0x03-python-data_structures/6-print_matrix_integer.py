c2c306e825f0a42809630ad44b3022e177656b1a
naxus1 <naxus1@gmail.com>
2019-05-07 19:36:53 +0000

    Created point 6

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -1,11 +1,11 @@
 #!/usr/bin/python3
 def print_matrix_integer(matrix=[[]]):
-    if len(matrix[0]) != 0:
+    if matrix == [[]]:
+        print()
+    else:
         for i in range(len(matrix)):
             for j in range(len(matrix)):
                 if j == len(matrix) - 1:
                     print('{:d}'.format(matrix[i][j]))
                 else:
                     print('{:d} '.format(matrix[i][j]), end='')
-    else:
-        print()
