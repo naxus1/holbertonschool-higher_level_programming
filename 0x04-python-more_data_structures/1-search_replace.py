2669f5bb3d7463f18fd859ba730bd3143f201443
naxus1 <naxus1@gmail.com>
2019-05-09 19:38:53 +0000

    modified: include for and if

--- 0x04-python-more_data_structures/1-search_replace.py
+++ 0x04-python-more_data_structures/1-search_replace.py
@@ -1,5 +1,7 @@
 #!/usr/bin/python3
 def search_replace(my_list, search, replace):
     new_list = my_list[:]
-    new_list[search - 1] = replace
+    for i in range(len(new_list)):
+        if new_list[i] == search:
+            new_list[i] = replace
     return new_list
