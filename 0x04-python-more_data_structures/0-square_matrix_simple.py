8b8f63181e651fb290a37f6e903b2b4ad2afdf88
naxus1 <naxus1@gmail.com>
2019-05-09 19:32:50 +0000

    modified: lambda

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
     new_list = matrix[:]
-    mult = lambda n: n * 2
     for i in range(len(matrix)):
-        new_list[i] = list(map(mult, matrix[i]))
+        new_list[i] = list(map(lambda n: n * n, matrix[i]))
     return new_list
