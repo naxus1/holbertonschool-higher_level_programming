64e6a2b0ab4914dc21d6abbd437aceb6064b92ad
naxus1 <naxus1@gmail.com>
2019-05-10 17:17:27 +0000

    Create point 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    dict_roman = {'I': 1, 'V': 5, 'X' : 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    num_ant = 0
+    suma = 0
+    for number in roman_string:
+        if dict_roman.get(number) <= num_ant:
+            suma += dict_roman.get(number)
+        else:
+            suma += (dict_roman.get(number) - (2 * num_ant))
+        num_ant = dict_roman.get(number)
+    return suma
