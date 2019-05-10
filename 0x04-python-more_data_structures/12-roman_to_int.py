856d10d1ed8229d9ba8b6e395d3511ecd35b856e
naxus1 <naxus1@gmail.com>
2019-05-10 17:24:10 +0000

    Fix: if , add len

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -3,7 +3,7 @@ def roman_to_int(roman_string):
     dict_roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,
                   'M': 1000}
 
-    if roman_string is None:
+    if roman_string is None or len(roman_string) == 0:
         return (0)
     num_ant = 0
     suma = 0
