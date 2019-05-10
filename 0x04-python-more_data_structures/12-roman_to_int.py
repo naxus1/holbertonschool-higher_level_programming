39167d278fc616730bdc8af76077e92a453fd414
naxus1 <naxus1@gmail.com>
2019-05-10 17:22:44 +0000

    Add conditional if

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -2,6 +2,9 @@
 def roman_to_int(roman_string):
     dict_roman = {'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500,
                   'M': 1000}
+
+    if roman_string is None:
+        return (0)
     num_ant = 0
     suma = 0
     for number in roman_string:
