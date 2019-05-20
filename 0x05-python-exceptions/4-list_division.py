c441a3a12acc3a28fc2560375f5b05d5cc0bd9c1
naxus1 <naxus1@gmail.com>
2019-05-20 23:09:07 +0000

    Point 4 created

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def list_division(my_list_1, my_list_2, list_length):
+    new_list = []
+    for num in range(0, list_length):
+        try:
+            result = my_list_1[num] / my_list_2[num]
+        except (TypeError):
+            print("wrong type")
+            result = 0
+            pass
+        except (ZeroDivisionError):
+            print("division by 0")
+            result = 0
+            pass
+        except (IndexError):
+            print("out of range")
+            result = 0
+            pass
+        finally:
+            new_list.append(result)
+    return new_list
