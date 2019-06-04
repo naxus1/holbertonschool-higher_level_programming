2f7cb392b23a06f15be9927f98d03d2481b7c6eb
naxus1 <naxus1@gmail.com>
2019-06-04 22:26:22 +0000

    Create point 10

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+"""Rectangle Subclass"""
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
