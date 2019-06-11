fdfae90b4a025bcbb56789bfd7b459286c74f701
naxus1 <naxus1@gmail.com>
2019-06-11 19:11:37 +0000

    update

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -6,10 +6,9 @@ import json
 
 class Rectangle(Base):
     """Rectangle class"""
-
     def __init__(self, width, height, x=0, y=0, id=None):
         "Initiation of the class with id"
-        Base.__init__(self, id)
+        super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
