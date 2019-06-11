b1fff30fd33f3784e2275b2fe3e3630472297c66
naxus1 <naxus1@gmail.com>
2019-06-11 18:51:40 +0000

    Update

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -1,21 +1,19 @@
 #!/usr/bin/python3
-
+"""Class base"""
 import json
 
-"""Class base"""
 
-
-class Base():
-
+class Base:
+    "Base class"
     __nb_objects = 0
 
     def __init__(self, id=None):
-
+        "Initiation of the class with id"
         if id is not None:
             self.id = id
         else:
             Base.__nb_objects += 1
-            self.id = Base.__nb_objects
+            self.id = self.__nb_objects
 
     @staticmethod
     def to_json_string(list_dictionaries):
