5212a2486a4d9529c6ca87778c18cb2ee2325d0c
naxus1 <naxus1@gmail.com>
2019-06-11 20:53:04 +0000

    update

--- 0x0C-python-almost_a_circle/models/square.py
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -1,34 +1,32 @@
 #!/usr/bin/python3
-"""Class Square"""
-import json
+"""Square class inherits from Rectangle"""
 from models.rectangle import Rectangle
 
 
 class Square(Rectangle):
-    """Class Square"""
-
+    """Defines a new square."""
     def __init__(self, size, x=0, y=0, id=None):
-        """Initiation of the class"""
+        """Initialitation of the class"""
         super().__init__(size, size, x, y, id)
-
-    def __str__(self):
-        """Return representation of Square"""
-        return("[Square] ({}) {}/{} - {}".format(self.id, self.x,
-                                                 self.y, self.width))
 
     @property
     def size(self):
-        """Return size of Square"""
+        """This method return the size of Square"""
         return self.width
 
     @size.setter
     def size(self, value):
-        """Sets value to height/width"""
+        """this method set value to height/width in size"""
         self.width = value
         self.height = value
 
+    def __str__(self):
+        """this method return the representation of square"""
+        return("[Square] ({}) {}/{} - {}".format(self.id, self.x,
+                                                 self.y, self.width))
+
     def update(self, *args, **kwargs):
-        """Update Square"""
+        """This method square comment"""
         if len(args) > 0 and args is not None:
             try:
                 self.id = args[0]
@@ -51,4 +49,5 @@ class Square(Rectangle):
                     self.y = value
 
     def to_dictionary(self):
+        """This method return a dictionary"""
         return {'size': self.width, 'id': self.id, 'x': self.x, 'y': self.y}
