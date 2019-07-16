830cecc3741124b8232fd62ab1938d8503e234c2
naxus1 <naxus1@gmail.com>
2019-07-15 21:13:06 -0500

    Create point 10 to 14

---
+++ 0x0D-SQL_introduction/12-no_cheating.sql
@@ -0,0 +1,5 @@
+-- cript that updates the score of Bob to 10
+UPDATE `second_table`
+SET
+`score` = 10
+WHERE `second_table`.`name` = 'Bob';
---
+++ 0x0D-SQL_introduction/14-average.sql
@@ -0,0 +1,3 @@
+-- Computes the score average of all records
+SELECT SUM(score)/COUNT(score) AS average
+FROM second_table;
