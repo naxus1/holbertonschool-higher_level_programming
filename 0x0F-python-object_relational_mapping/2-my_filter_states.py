909ebbad3098702198a0af5a76abd690c2a93969
naxus1 <naxus1@gmail.com>
2019-08-11 00:53:17 +0000

    Modified query

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,8 +1,6 @@
 #!/usr/bin/python3
 
-"""
-Write a script that takes in an argument and displays all values
-in the states table of hbtn_0e_0_usa where name matches the argument.
+"""Write a script that takes in an argument and displays all values
 """
 
 from sys import argv
@@ -17,9 +15,8 @@ if __name__ == "__main__":
                          db=argv[3])
 
     cur = db.cursor()
-    cur = execute("""SELECT * FROM states
-    WHERE name=%s
-    ORDER BY id""", (argv[4],))
+    cur.execute("SELECT id, name FROM states WHERE BINARY NAME='{}'\
+    ORDER BY id ASC".format(argv[4]))
 
     allData = cur.fetchall()
     for row in allData:
