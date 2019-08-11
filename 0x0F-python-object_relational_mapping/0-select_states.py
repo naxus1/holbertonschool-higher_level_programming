9dcc56fb44e88306fda4dc877628528c07e1054b
naxus1 <naxus1@gmail.com>
2019-08-11 00:05:13 +0000

    Fix PEP8

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -1,16 +1,15 @@
 #!/usr/bin/env python3
 """script that lists all states from the database hbtn_0e_0_usa"""
-
 from sys import argv
 import MySQLdb
 
 if __name__ == "__main__":
-    db=MySQLdb.connect(host="localhost",
-                       port=3306,
-                       user=argv[1],
-                       passwd=argv[2],
-                       db=argv[3])
-    cur_sor=db.cursor()
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    cur_sor = db.cursor()
     cur_sor.execute("SELECT * FROM states ORDER BY id ASC")
     all_data = cur_sor.fetchall()
     for row in all_data:
