04995b65a615e2d1751b01ca8c2bd408cdce499f
naxus1 <naxus1@gmail.com>
2019-08-11 01:27:13 +0000

    FIX bd close

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,20 +1,21 @@
 #!/usr/bin/python3
 """
-safe from MySQL injections
+write a script that takes in arguments and displays all values
 """
+
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(
-        host='localhost',
-        user=argv[1],
-        passwd=argv[2],
-        db=argv[3]
-    )
-
-    access = db.cursor()
-    access.execute("SELECT * FROM states WHERE states.name = %s", (argv[4],))
-    rows = access.fetchall()
-    for row in rows:
+    db = MySQLdb.connect(host="localhost",
+                         port=3306,
+                         user=argv[1],
+                         passwd=argv[2],
+                         db=argv[3])
+    cur = db.cursor()
+    cur.execute("SELECT * FROM states WHERE name=%s ORDER BY id", (argv[4],))
+    allData = cur.fetchall()
+    for row in allData:
         print(row)
+    cur.close()
+    bd.close()
