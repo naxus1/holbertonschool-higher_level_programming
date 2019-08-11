05798ddf2b080918600bb114e263cad76a285890
naxus1 <naxus1@gmail.com>
2019-08-11 22:29:44 +0000

    Fix query

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,9 +1,6 @@
 #!/usr/bin/python3
 
-"""
-Write a python file that contains the class
-definition of a State and an instance
-Base = declarative_base():
+"""Create Table
 """
 
 from sqlalchemy import Column, Integer, String
@@ -11,10 +8,16 @@ from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
 
+
 class State(Base):
-    """Definitios of State"""
+    """
+    Class definitios of State
+    """
 
     __tablename__ = 'states'
-    id = Column(Integer, nullable=False, unique=True,
-                autoincrement=True, primary_key=True)
+    id = Column(Integer,
+                nullable=False,
+                unique=True,
+                autoincrement=True,
+                primary_key=True)
     name = Column(String(128), nullable=False)
