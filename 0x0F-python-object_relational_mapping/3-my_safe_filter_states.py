4e6820c0edb53aaca4c3d335b588c9b5f32c8777
naxus1 <naxus1@gmail.com>
2019-08-11 01:21:46 +0000

    Modified

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,21 +1,20 @@
 #!/usr/bin/python3
 """
-write a script that takes in arguments and displays all values
+safe from MySQL injections
 """
-
 import MySQLdb
 from sys import argv
 
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost",
-                         port=3306,
-                         user=argv[1],
-                         passwd=argv[2],
-                         db=argv[3])
-    cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE states.name = %s", (argv[4],))
-    allData = cur.fetchall()
-    for row in allData:
+    db = MySQLdb.connect(
+        host='localhost',
+        user=argv[1],
+        passwd=argv[2],
+        db=argv[3]
+    )
+
+    access = db.cursor()
+    access.execute("SELECT * FROM states WHERE states.name = %s", (argv[4],))
+    rows = access.fetchall()
+    for row in rows:
         print(row)
-    cur.close()
-    conn.close()
