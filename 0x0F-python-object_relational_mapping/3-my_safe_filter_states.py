44ed26b95b068e1fed5141694f5405f5b662fd54
naxus1 <naxus1@gmail.com>
2019-08-12 01:26:04 +0000

    Create points

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -1,19 +1,20 @@
 #!/usr/bin/python3
-"""
-write a script that takes in arguments and displays all values
+"""script SQL inyection that takes in arguments
 """
 
-import MySQLdb
-from sys import argv
 
 if __name__ == "__main__":
+
+    import MySQLdb
+    from sys import argv
+
     db = MySQLdb.connect(host="localhost",
                          port=3306,
                          user=argv[1],
                          passwd=argv[2],
                          db=argv[3])
     cur = db.cursor()
-    cur.execute("SELECT * FROM states WHERE name=%s ORDER BY id", (argv[4],))
+    cur.execute("SELECT * FROM states WHERE states.name = %s", (argv[4],))
     allData = cur.fetchall()
     for row in allData:
         print(row)
