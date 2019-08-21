57444a8a296db532a2271449ff593e42ef6d7565
naxus1 <naxus1@gmail.com>
2019-08-21 02:10:07 +0000

    Fix point 3

--- 0x10-python-network_0/3-methods.sh
+++ 0x10-python-network_0/3-methods.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-# Bash script that displays all HTTP methods the server will accept
-curl -sI "$1" | grep Allow | cut -d ":" -f2 | sed -e 's/^[ \t]*//'
+ #Displays the size of the body of the response
+curl -sI $1 | head -4 | tail -1 | cut -d":" -f2 | sed 's/ //'
