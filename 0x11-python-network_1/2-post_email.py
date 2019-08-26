fcbd5d8af32ff33bf2bf4445455ec3a739c84db2
naxus1 <naxus1@gmail.com>
2019-08-26 19:59:35 +0000

    Create points 0 to 2

---
+++ 0x11-python-network_1/2-post_email.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+# Python script that takes in a URL and an email, sends a POST to email
+
+from urllib import parse, request
+import sys
+
+if __name__ == "__main__":
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+    values = {"email":email}
+    data = parse.urlencode(values).encode('ascii')
+    req = request.Request(url, data)
+    with request.urlopen(req) as response:
+        the_page = response.read().decode('utf-8')
+    print(the_page)
