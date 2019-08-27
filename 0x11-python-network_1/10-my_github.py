8b6d67e5b2018e5cac663f09f0f36408e8f04789
naxus1 <naxus1@gmail.com>
2019-08-27 01:09:38 +0000

    Modified print

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -9,4 +9,4 @@ if __name__ == "__main__":
     url = "https://api.github.com/users/" + sys.argv[1]
     logi = requests.get(url, auth=(sys.argv[1], sys.argv[2]))
     logi_dict = logi.json()
-    print(logi_dict['id'])
+    print(logi_dict.get('id'))
