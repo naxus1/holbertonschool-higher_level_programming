eaf1e426ace5d49927eda1bd90a2743e0e1a2a30
naxus1 <naxus1@gmail.com>
2019-09-17 20:37:15 +0000

    Fix - change variable

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
-let num = process.argv[2];
-if (!isNaN(parseInt(myNum))) {
-  console.log('My number: %d', (myNum));
-} else {
+let num = parseInt(process.argv[2], 10);
+if (isNaN(num) === true) {
   console.log('Not a number');
+} else {
+  console.log('My number: ' + num);
 }
