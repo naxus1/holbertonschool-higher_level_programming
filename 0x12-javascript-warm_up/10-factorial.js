952f657eb027bc0c05256a6844de5a4ffb38c9ff
naxus1 <naxus1@gmail.com>
2019-09-17 23:56:53 +0000

    Create points 10 and 11

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+let numFact = process.argv.slice(2);
+let res = 0;
+
+function factorial (n) {
+  if (n === 1) {
+    return 1;
+  } else {
+    return (factorial(n - 1) * n);
+  }
+}
+if (!numFact[0]) {
+  res = 1;
+} else {
+  res = factorial(numFact[0]);
+}
+console.log(res);
---
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const arraNums = process.argv.slice(2);
+let maxNum = 0;
+
+if (arrayNums.length > 1) {
+  arrayNums.sort();
+  maxNum = arrayNums[arrayNums.length - 2];
+}
+console.log(maxNum);
