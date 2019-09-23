0401b04df48ca5a419fff7217208b1cdd126cf96
naxus1 <naxus1@gmail.com>
2019-09-23 21:18:45 +0000

    Fix - its change de closule mudule at the end

--- 0x13-javascript_objects_scopes_closures/4-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
-module.exports = class Rectangle {
+class Rectangle {
   constructor (w, h) {
     if (w > 0 && h > 0) {
       this.width = w;
@@ -18,7 +18,9 @@ module.exports = class Rectangle {
   }
 
   double () {
-    this.width *= this.width;
-    this.height *= this.height;
+    this.width = this.width * 2;
+    this.height = this.height * 2;
   }
-};
+}
+
+module.exports = Rectangle;
