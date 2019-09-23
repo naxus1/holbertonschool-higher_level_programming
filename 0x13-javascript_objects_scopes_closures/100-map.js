8e39e3150dee9dccad0dc4ab22c10e824235ad53
naxus1 <naxus1@gmail.com>
2019-09-23 22:41:13 +0000

    Fix - Modified function return

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -1,7 +1,5 @@
 #!/usr/bin/node
-const myList = require('100-data.js').list;
+const myList = require('./100-data.js').list;
 console.log(myList);
-let myArray = mylist.map(function (x, i) {
-  return x * i;
-});
+const myArray = myList.map((curray, start) => curray * start);
 console.log(myArray);
