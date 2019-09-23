f5f2084ccea5518f3c8f4426f30863d860d52b19
naxus1 <naxus1@gmail.com>
2019-09-23 21:07:34 +0000

    The points 0 to 6 were created

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+const mysquare = require('./5-square');
+module.exports = class Square extends mysquare {
+  charPrint (c) {
+    if (c === undefined) {
+      c = 'X';
+    }
+    for (let i = 0; i < this.height; i++) {
+      console.log(c.repeat(this.width));
+    }
+  }
+};
