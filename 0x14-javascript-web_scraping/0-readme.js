6a435c5d6dd853897abe0d2df6d91334cd754fa6
naxus1 <naxus1@gmail.com>
2019-09-24 19:38:51 +0000

    Fix - rename variable fs

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,8 +1,8 @@
 #!/usr/bin/node
-/* Write a script that reads and prints the content of a file. */
-const fs = require('fs');
+/* script that reads and prints the content of a file. */
+const f = require('fs');
 
-fs.readFile(process.argv[2], 'utf-8', (err, data) => {
+f.readFile(process.argv[2], 'utf-8', (err, data) => {
   if (err) {
     console.log(err);
   } else {
