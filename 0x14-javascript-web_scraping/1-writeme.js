239b71348b27b0ff63a8b1aece6718cc9fd27f9c
naxus1 <naxus1@gmail.com>
2019-09-24 19:56:14 +0000

    Fix name variables

--- 0x14-javascript-web_scraping/1-writeme.js
+++ 0x14-javascript-web_scraping/1-writeme.js
@@ -1,9 +1,6 @@
 #!/usr/bin/node
-/* Write a script that writes a string to a file. */
-const f = require('fs');
-const myFile = process.argv[2];
-const content = process.argv[3];
-f.writeFile(myFile, content, 'utf-8', (err, data) => {
+const file = require('fs');
+file.writeFile(process.argv[2], process.argv[3], 'utf-8', (err, data) => {
   if (err) {
     console.log(err);
   }
