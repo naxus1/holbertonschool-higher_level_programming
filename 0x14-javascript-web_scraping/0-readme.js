6eba5132aa65e75fa3c00fa88caadaee070f930c
naxus1 <naxus1@gmail.com>
2019-09-24 20:04:45 +0000

    Fix name variables

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -2,8 +2,8 @@
 const fs = require('fs');
 fs.readFile(process.argv[2], 'utf-8', (err, data) => {
   if (err) {
-    console.log(err);
+    return console.log(err);
   } else {
     console.log(data);
   }
-);
+});
