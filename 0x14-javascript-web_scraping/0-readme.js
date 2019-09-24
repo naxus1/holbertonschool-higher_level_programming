78cfe8f9480dc170dea8157b950dac7723c98038
naxus1 <naxus1@gmail.com>
2019-09-24 19:36:39 +0000

    Fix - Its modified utf and add comment

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
-
+/* Write a script that reads and prints the content of a file. */
 const fs = require('fs');
 
-fs.readFile(process.argv[2], 'utf8', (err, data) => {
+fs.readFile(process.argv[2], 'utf-8', (err, data) => {
   if (err) {
-    console.error(err);
+    console.log(err);
   } else {
     console.log(data);
   }
