95816bea35d02b26a05369f64514bf223647749a
naxus1 <naxus1@gmail.com>
2019-10-03 17:32:31 +0000

    Fix - Create point9

--- 0x15-javascript-web_jquery/9-script.js
+++ 0x15-javascript-web_jquery/9-script.js
@@ -1,5 +1,3 @@
-$.get('https://query.yahooapis.com/v1/public/yql?q=select%20wind%20from%20weather.forecast%20where%20woeid%20in%20(select%20woeid%20from%20geo.places(1)%20where%20text%3D%22:city_name%22)&format=json', function (data) {
-  let windSpeed = (data['query']['results']['channel']['wind']['speed']);
-
-  $('#sf_wind_speed').text(windSpeed);
+$.getJSON('https://fourtonfish.com/hellosalut/?lang=fr', function (data) {
+  $('#hello').text(data.hello);
 });
